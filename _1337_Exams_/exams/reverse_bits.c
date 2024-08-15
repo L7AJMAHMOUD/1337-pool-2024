e28ee9ab53989672407eced34481ec8186b8dd31
_L7AJ_MAHMOUD_ <130211556+L7AJMAHMOUD@users.noreply.github.com>
2024-08-15 12:10:56 +0100

    Add files via upload

---
+++ _1337_Exams_/exams/reverse_bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int i;
+	unsigned char result;
+
+	i = 8;
+	result = 0;
+	while (i > 0)
+	{
+		result = result * 2 + (octet % 2);
+		octet /= 2;
+		i--;
+	}
+	return (result);
+}
